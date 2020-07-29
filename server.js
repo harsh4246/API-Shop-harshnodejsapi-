8467399356eb7854b149573047070d7c2064aeac
Harshvardhan <hydshiva2010@gmail.com>
2020-07-30 02:51:34 +0530

    updated port

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const http=require('http');
 const app=require('./app');
 
 
-//const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-//server.listen(port);
-server.listen(3000,'192.168.43.96');
+server.listen(port);
