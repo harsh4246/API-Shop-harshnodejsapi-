be4ab67ae472f93735b4e51b8e929bef6c52acce
Harshvardhan <hydshiva2010@gmail.com>
2020-07-31 01:53:35 +0530

    update

--- api/routes/products.js
+++ api/routes/products.js
@@ -34,7 +34,46 @@ const upload=multer({
         else{
             console.log('file is not clean');
             cb(null, false);
-        }
+        }const upload=multer({
+
+            storage:storage,
+        
+            limits:{
+                fileSize: 1024*1024*5
+            },
+        
+            fileFilter:function(req,file,cb){
+                if(file.mimetype==='image/jpeg' || file.mimetype==='image/png'){
+                    console.log('file is clean');
+                    cb(null, true);
+                }
+                else{
+                    console.log('file is not clean');
+                    cb(null, false);
+                }
+        
+            }
+        
+        });
+        
+        
+        
+        
+        router.get('/',productsController.products_get_all);
+        
+        router.post('/',checkAuth,upload.single('productImage'),productsController.products_create_one);
+        
+        router.get('/info',productsController.products_post_info);
+        
+        router.get('/:productId',productsController.products_get_one);
+        
+        router.patch('/:productId',checkAuth,productsController.products_edit_one);
+        
+        router.get('/:productId/info',productsController.products_patch_info);
+        
+        router.delete('/:productId',checkAuth,productsController.products_delete_one);
+        
+        module.exports=router;
 
     }
 
