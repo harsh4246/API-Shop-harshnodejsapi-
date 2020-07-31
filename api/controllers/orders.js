832b677bf8f65cbcfafd076bd2ac47031a93faf9
Harshvardhan <hydshiva2010@gmail.com>
2020-08-01 02:39:53 +0530

    update

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -15,6 +15,7 @@ exports.orders_get_all=(req,res,next)=>{
                 return {
                     _id:doc._id,
                     product:doc.product,
+                    User:req.userData,
                     quantity:doc.quantity,
                     requests:{
                         type:'GET',
